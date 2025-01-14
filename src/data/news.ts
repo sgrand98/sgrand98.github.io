0131d5acf5cea0a32e18dfcfec9c6d693be9078b
SebastienGrand98 <sebastiengrdmtz@gmail.com>
2025-01-14 21:54:24 +0100

    hide port. and pub.

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,10 +7,5 @@ export interface News {
 
 export const newsData: News[] = [
   // If you don't want to show news, just make the array empty.
-  /*{
-    date: "March 2024",
-    title: "Paper accepted at ICML 2024",
-    description: "Our work on causal discovery in time series data has been accepted at ICML 2024.",
-    link: "https://icml.cc/",
-  }*/
+
 ];

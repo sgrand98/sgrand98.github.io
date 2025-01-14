dd397dcad032ac1b2c2faa43abfc207b8ac19bae
SebastienGrand98 <sebastiengrdmtz@gmail.com>
2025-01-14 21:42:33 +0100

    modified layout.tsx

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -6,7 +6,7 @@ export interface AboutMe {
   name: string;
   title: string;
   institution: string;
-  description: string[];
+  description: string;
   email: string;
   imageUrl?: string | StaticImageData;
   blogUrl?: string;
@@ -25,15 +25,13 @@ export const aboutMe: AboutMe = {
   name: "Sebastien Grand",
   title: "Ph.D. Student",
   institution: "IMT Mines Albi",
-  description: [
-    `<p style='font-size: larger; text-align: justify;'>Hi there, I am <b>Sébastien Grand</b>!<br><br>
+  description: `<p style='font-size: larger; text-align: justify;'>Hi there, I am <b>Sébastien Grand</b>!<br><br>
     I'm a 1st year PhD student at <a href='https://www.imt-mines-albi.fr/'>IMT Mines Albi</a>
     working on <b>robust and adaptive Deep Multimodal Fusion</b>.<br><br>
     Previously, I received a <b>M.S in Computer Science and Mathematics</b>, a double diploma from 
     <a href='https://www.enseeiht.fr/fr/formation/formation-ingenieur/departement-sn/programme-sn/parcours-modia.html'>INP ENSEEIHT</a> and
     <a href='https://www.math.insa-toulouse.fr/fr/enseignement/apprentissage-modia.html/'>INSA Toulouse</a> in 2023.<br><br>
-    Then, I worked for a year at <a href='https://epsi-radars.fr/'>EPSI Radar</a> at developing Computer Vision classification models for radar data.</p>`
-  ],
+    Then, I worked for a year at <a href='https://epsi-radars.fr/'>EPSI Radar</a> at developing Computer Vision classification models for radar data.</p>`,
   email: "sebastien.grand@mines-albi.fr",
   imageUrl: photo,
   githubUsername: "SebastienGrand98",

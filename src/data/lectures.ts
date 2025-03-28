d0fc9016e919a50ba82579f1655de8929c35eb83
SebastienGrand98 <sebastiengrdmtz@gmail.com>
2025-03-28 09:13:44 +0100

    add lectures

--- src/data/lectures.ts
+++ src/data/lectures.ts
@@ -10,5 +10,11 @@ export const lecturesData: Lectures[] = [
     {
         course: "IMT Mines Albi, Master 1 - option Data Science, Introduction to Machine Learning, 39h",
     },
+    {
+        course: "Data Analysis Course, Advanced Techniques for Statistical Data Analysis, 10h",
+    },
+    {
+        course: "Production Management Workshop, Fundamentals of Production Systems, 15h",
+    },
     // Add more lecture entries as needed
 ];
